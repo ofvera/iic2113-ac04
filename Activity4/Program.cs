fb1976835f6cb4f956594043436c8ad0fd5f21f2
Octavio Vera <ofvera@uc.cl>
2019-12-08 05:27:13 -0300

    added other exchanges and show in program

--- Activity4/Program.cs
+++ Activity4/Program.cs
@@ -14,10 +14,19 @@ namespace Activity4
             // Console.WriteLine($"{clp.Value} CLP equals {usd.Value} USD through Exchange1.");
 
             Currency clp = new CLPCurrency(100000);
-            CurrencyExchange exchange = new Exchange1();
+            Currency usd = new USDCurrency(300);
+            Currency eur = new EURCurrency(400);
+            CurrencyExchange exchange1 = new Exchange1();
+            CurrencyExchange exchange2 = new Exchange2();
             IConvertor pesoDolarConvertor = new ConvertPesoDolar();
-
-            var usd = exchange.Exchange(clp, pesoDolarConvertor);
+            IConvertor dolarEuroConvertor = new ConvertDolarEuro();
+            IConvertor euroPesoConvertor = new ConvertEuroPeso();
+            var clp_usd = exchange1.Exchange(clp, pesoDolarConvertor);
+            var usd_eur = exchange1.Exchange(usd, dolarEuroConvertor);
+            var eur_clp = exchange1.Exchange(eur, euroPesoConvertor);
+            Console.WriteLine("Used Exchange1 from CLP: " + clp.Value + " to USD: " + clp_usd.Value);
+            Console.WriteLine("Used Exchange1 from USD: " + usd.Value + " to EUR: " + usd_eur.Value);
+            Console.WriteLine("Used Exchange1 from EUR: " + eur.Value + " to CLP: " + eur_clp.Value);
         }
     }
 }
